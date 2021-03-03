9736612e1cf93f212c4fd1d5546383bc016554aa
junghyun park <junghyun.park@wadiz.kr>
2021-03-03 23:21:25 +0900

    Setup UI Framework Grommet

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Head from 'next/head';
 import Layout from '@/components/layout';
+import { Grommet } from 'grommet';
 
 export default function MyApp({ Component, pageProps }) {
+  const theme = {
+    global: {
+      font: {
+        family: 'Roboto',
+        size: '18px',
+        height: '20px',
+      },
+    },
+  };
   return (
     <Layout>
       <Head>
+        <link rel = "stylesheet"href = "https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
         <title>Jab</title>
       </Head>
-      <Component {...pageProps} />
+      <Grommet theme={theme} >
+        <Component {...pageProps} />
+      </Grommet>
     </Layout>
   );
 }
