98799fdbe18ecb623b211fa79734d0ad4a5aea13
junghyun park <junghyun.de@gmail.com>
2021-03-22 01:28:59 +0900

    Add Copy

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import { Card, CardHeader, CardBody, CardFooter, Button, Heading, Paragraph } from 'grommet';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+  Heading,
+  Paragraph,
+} from 'grommet';
 import { Favorite, ShareOption } from 'grommet-icons';
+import Link from 'next/link';
 
 const ProjectCard = ({ title, description, categorys = [] }) => {
   const badgeStyle = {
@@ -11,47 +21,66 @@ const ProjectCard = ({ title, description, categorys = [] }) => {
     backgroundColor: '#ddd',
   };
 
- const cardTextSyle = {
+  const cardTextSyle = {
     title: {
-      'overflow': 'hidden',
-      'text-overflow': 'ellipsis',
-      'margin': '0 0 4px 0',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      margin: '0 0 4px 0',
       'word-break': 'keep-all',
-      'color': '#111',
+      color: '#111',
     },
     description: {
-      'display': '-webkit-box',
-      'WebkitLineClamp': '2',
-      'WebkitBoxOrient': 'vertical',
-      'whiteSpace': 'normal',
-      'overflow': 'hidden',
-      'textOverflow': 'ellipsis',
-      'margin': '0 0 20px 0',
-      'wordBreak': 'break-all'
+      display: '-webkit-box',
+      WebkitLineClamp: '2',
+      WebkitBoxOrient: 'vertical',
+      whiteSpace: 'normal',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      margin: '0 0 20px 0',
+      wordBreak: 'break-all',
     },
- }
+  };
 
   return (
-    <Card height="medium" margin="medium" width="auto" background="light-1">
-      <CardHeader
-        pad="medium"
-        height="small"
-        style={{
-          background: 'url("https://via.placeholder.com/500") no-repeat 50%/cover',
-        }}
-      ></CardHeader>
+    <Card
+      height="medium"
+      margin="medium"
+      width="auto"
+      height="auto"
+      background="light-1"
+    >
+      <Link href="/asdaf">
+        <a href="/asdaf">
+          <CardHeader
+            pad="medium"
+            height="small"
+            style={{
+              background:
+                'url("https://via.placeholder.com/500") no-repeat 50%/cover',
+            }}
+          />
 
-      <CardBody pad="medium" style={{ display: 'block' }}>
-        <Heading style={cardTextSyle.title} level="3">{title}</Heading>
-        <Paragraph style={cardTextSyle.description}>{description}</Paragraph>
-        {categorys.length > 0 && categorys.map((label) => {
-          <span style={badgeStyle}>{label}</span>  
-        })}
-      </CardBody>
+          <CardBody pad="medium" style={{ display: 'block' }}>
+            <Heading style={cardTextSyle.title} level="3">
+              {title}
+            </Heading>
+            
+            <Paragraph style={cardTextSyle.description}>
+              {description}
+            </Paragraph>
 
+            {categorys.length > 0 &&
+              categorys.map((label) => {
+                <span style={badgeStyle}>{label}</span>;
+              })}
+          </CardBody>
+        </a>
+      </Link>
       <CardFooter pad={{ horizontal: 'small' }} background="light-2">
         <Button icon={<Favorite color="red" />} hoverIndicator />
-        <Button icon={<ShareOption color="plain" />} hoverIndicator />
+        <CopyToClipboard text="/asdfa" onCopy={() => alert('링크가 복사되었습니다.')}>
+          <Button icon={<ShareOption color="plain" />} hoverIndicator>링크 공유하기</Button>
+        </CopyToClipboard>
       </CardFooter>
     </Card>
   );
--- components/ProjectList.jsx
+++ components/ProjectList.jsx
@@ -1,52 +1,58 @@
-import React from 'react'
+import React from 'react';
 import ProjectCard from './ProjectCard';
 
 const PROJECT_DATA = [
   {
-    "title": "This is just title for test",
-    "description": "This is just description for test",
-    "categorys": ["development, design"]
+    title: 'This is just title for test',
+    description: 'This is just description for test',
+    categorys: ['development, design'],
   },
   {
-    "title": "This is just title for test",
-    "description": "This is just description for test",
-    "categorys": ["development, design"]
+    title: 'This is just title for test',
+    description: 'This is just description for test',
+    categorys: ['development, design'],
   },
   {
-    "title": "This is just title for test",
-    "description": "This is just description for test",
-    "categorys": ["development, design"]
+    title: 'This is just title for test',
+    description: 'This is just description for test',
+    categorys: ['development, design'],
   },
   {
-    "title": "This is just title for test",
-    "description": "This is just description for test",
-    "categorys": ["development, design"]
+    title: 'This is just title for test',
+    description: 'This is just description for test',
+    categorys: ['development, design'],
   },
   {
-    "title": "This is just title for test",
-    "description": "This is just description for test",
-    "categorys": ["development, design"]
+    title: 'This is just title for test',
+    description: 'This is just description for test',
+    categorys: ['development, design'],
   },
   {
-    "title": "This is just title for test",
-    "description": "This is just description for test",
-    "categorys": ["development, design"]
-  }
-]
+    title: 'This is just title for test',
+    description: 'This is just description for test',
+    categorys: ['development, design'],
+  },
+];
 
 const projectListStyle = {
   display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)'
-}
+  gridTemplateColumns: 'repeat(auto-fit, minmax(min(360px, 100%), 1fr))',
+  paddingBottom: '80px'
+};
 
 const ProjectList = () => {
   return (
     <div style={projectListStyle}>
-      {PROJECT_DATA.map(({ title, description, categorys }) => (
-        <ProjectCard key={title} title={title} description={description} categorys={categorys} />  
+      {PROJECT_DATA.map(({ title, description, categorys }, index) => (
+        <ProjectCard
+          key={title + index}
+          title={title}
+          description={description}
+          categorys={categorys}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProjectList
+export default ProjectList;
