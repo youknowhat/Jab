5b28007c561350df9691143397bbe4dd2830b8cf
박정현 <junghyun.de@gmail.com>
2021-04-08 23:40:14 +0900

    메타 태그 별도의 파일로 분리

---
+++ components/Meta.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <title>Jab</title>
+    <meta
+      key="viewport"
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta
+      name="description"
+      content="Side Project Platform Jab"
+    />
+    <meta property="og:title" content="Jab" />
+    <meta
+      property="og:description"
+      content="Side Project Platform Jab"
+    />
+    <meta
+      property="og:image"
+      content="https://repository-images.githubusercontent.com/201392697/5d392300-eef3-11e9-8e20-53310193fbfd"
+    />
+  </Head>
+)
+
+export default Meta;
