aad439593ce8cd59761d124edf655e97fe58c1a4
박정현 <junghyun.de@gmail.com>
2021-04-21 22:28:19 +0900

    Edit 페이지 반응형 개발

--- components/Edit.jsx
+++ components/Edit.jsx
@@ -1,209 +1,170 @@
 import { useState } from 'react';
-import { Heading, Form, FormField, Button, CheckBoxGroup } from 'grommet';
+import {
+  Heading,
+  Form,
+  FormField,
+  Button,
+  CheckBoxGroup,
+  RadioButtonGroup,
+  Select,
+  TextArea,
+  Grommet,
+  ResponsiveContext,
+} from 'grommet';
 import Head from 'next/head';
 
 function Edit() {
   const [errorMsg, setErrorMsg] = useState('');
 
-  function handleSubmit() {
-    console.log('submit');
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert('submit');
   }
   return (
-    <>
-      <Head>
-        <title>프로젝트 생성하기</title>
-      </Head>
-      <Form
-        onSubmit={handleSubmit}
-        style={{ maxWidth: '600px', margin: '0 auto' }}
-      >
-        <Heading level="2" margin={{ vertical: '24px' }} fill>
-          작성하기
-        </Heading>
-        {errorMsg ? <p style={{ color: 'red' }}>{errorMsg}</p> : null}
-        {/* 닉네임 */}
-        <FormField
-          id="nickname"
-          type="text"
-          name="nickname"
-          label="닉네임"
-          required={true}
-          margin={{ top: '48px' }}
-        />
-
-        {/* 요약 */}
-        <FormField
-          id="summary"
-          type="password"
-          name="summary"
-          label="프로젝트 요약"
-          required={true}
-          margin={{ top: '48px' }}
-        />
-
-        {/* 설명 */}
-        <FormField
-          id="description"
-          type="text"
-          name="description"
-          label="프로젝트 설명"
-          required={true}
-          margin={{ top: '48px' }}
-        />
-
-        {/* 타입 */}
-        <FormField
-          id="projectType"
-          name="projectType"
-          label="프로젝트 타입"
-          required={true}
-          margin={{ top: '48px' }}
+    <ResponsiveContext.Consumer>
+      {(size) => (
+        <div
+          style={{
+            backgroundColor: '#eee',
+            padding: size !== 'small' ? '80px 0' : '16px',
+          }}
         >
-          <CheckBoxGroup options={['Maui', 'Kauai', 'Oahu']} />
-        </FormField>
+          <Head>
+            <title>시작하기</title>
+          </Head>
+          <Form
+            onSubmit={handleSubmit}
+            style={
+              size !== 'small'
+                ? {
+                    maxWidth: '600px',
+                    margin: '0 auto',
+                    backgroundColor: '#fff',
+                    borderRadius: '10px',
+                    width: '80%',
+                    padding: '80px',
+                  }
+                : {
+                    backgroundColor: '#fff',
+                    borderRadius: '10px',
+                    margin: '16px 0',
+                    padding: '16px',
+                    boxShadow: '0 8px 16px 10px rgb(0, 0, 0, 0.05)',
+                  }
+            }
+          >
+            <Heading level="2" margin={{ vertical: '24px' }}>
+              이런 걸 해보려고 해요
+            </Heading>
 
-        {/* 진행 상태 */}
-        <FormField
-          id="projectStatus"
-          name="projectStatus"
-          label="프로젝트 진행 상태"
-          required={true}
-          margin={{ top: '48px' }}
-        >
-          <Select options={['small', 'medium', 'large']} />
-        </FormField>
+            <FormField label="참여 가능 여부" margin={{ top: '48px' }}>
+              <RadioButtonGroup
+                name="attend"
+                options={['가능', '대기', '불가']}
+              />
+            </FormField>
 
-        {/* 현재 인원 */}
-        <FormField
-          id="projectStatus"
-          name="projectStatus"
-          label="프로젝트 현재 인원"
-          required={true}
-          margin={{ top: '48px' }}
-        >
-          <TextArea
-            placeholder="현재 프로젝트에 참여중인 사람들을 알려주세요."
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-            focusIndicator
-          />
-        </FormField>
+            <FormField
+              id="nickname"
+              type="text"
+              name="nickname"
+              label="닉네임"
+              placeholder="ex) 열정맨"
+              required={true}
+            />
 
-        {/* 목표 */}
-        <FormField
-          id="projectStatus"
-          name="projectStatus"
-          label="프로젝트 목표"
-          required={true}
-          margin={{ top: '48px' }}
-        >
-          <TextArea
-            placeholder="이 프로젝트의 목표를 알려주세요."
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-            focusIndicator
-          />
-        </FormField>
+            <FormField
+              id="summary"
+              name="summary"
+              label="프로젝트 요약"
+              placeholder="프로젝트를 간단히 요약해서 알려주세요."
+              required={true}
+            />
 
-        {/* 참여 연락 수단 */}
-        <FormField
-          id="projectStatus"
-          name="projectStatus"
-          label="참여 연락 수단"
-          required={true}
-          margin={{ top: '48px' }}
-        >
-          <TextArea
-            placeholder="프로젝트에 참여하기 위한 연락 수단을 알려주세요. (ex.이메일)"
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-            focusIndicator
-          />
-        </FormField>
+            <FormField
+              id="description"
+              name="description"
+              label="프로젝트 설명"
+              required={true}
+            >
+              <TextArea placeholder="프로젝트를 상세히 설명해주세요." />
+            </FormField>
 
-        {/* 참고 링크 */}
-        <FormField
-          id="projectStatus"
-          name="projectStatus"
-          label="참고 링크"
-          margin={{ top: '48px' }}
-        >
-          <TextArea
-            placeholder="프로젝트에 참여 전 참고할만한 링크를 알려주세요. (ex.나의 프로필, 진행중인 프로젝트 링크 등)"
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-            focusIndicator
-          />
-        </FormField>
+            <FormField
+              id="projectType"
+              name="projectType"
+              label="프로젝트 타입"
+              required={true}
+              margin={{ top: '48px' }}
+            >
+              <CheckBoxGroup options={['프로젝트', '스터디']} />
+            </FormField>
 
-        <Heading level="3" margin={{ vertical: '24px' }} fill>
-          이런 분을 찾고 있어요.
-        </Heading>
+            <FormField
+              id="skill"
+              name="skill"
+              label="필요한 스킬"
+              required={true}
+              margin={{ top: '48px' }}
+            >
+              <CheckBoxGroup
+                options={['기획', '개발', '마케팅', '디자인', '기타']}
+              />
+            </FormField>
 
-        {/* 참여 가능 여부 */}
-        <FormField
-          id="projectStatus"
-          name="projectStatus"
-          label="참여 가능 여부"
-          margin={{ top: '48px' }}
-        >
-          <RadioButtonGroup
-            name="doc"
-            options={['one', 'two']}
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-          />
-        </FormField>
+            <FormField
+              id="status"
+              name="status"
+              label="프로젝트 진행 상태"
+              required={true}
+              margin={{ top: '48px' }}
+            >
+              <Select
+                options={['시작 전', '초반', '중반', '완료']}
+                defaultValue="시작 전"
+              />
+            </FormField>
 
-        {/* 관련 경험 */}
-        <FormField
-          id="projectStatus"
-          name="projectStatus"
-          label="관련 경험"
-          margin={{ top: '48px' }}
-        >
-          <TextArea
-            placeholder="이 프로젝트와 관련된 경험을 알려주세요."
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-            focusIndicator
-          />
-        </FormField>
+            <FormField
+              id="experience"
+              name="experience"
+              label="관련 경험"
+              margin={{ top: '48px' }}
+            >
+              <TextArea placeholder="관련된 경험이 있다면 알려주세요." />
+            </FormField>
 
-        {/* 필요 스킬 */}
-        <FormField
-          id="projectType"
-          name="projectType"
-          label="필요한 스킬"
-          required={true}
-          margin={{ top: '48px' }}
-        >
-          <CheckBoxGroup options={['Maui', 'Kauai', 'Oahu']} />
-        </FormField>
-        <FormField
-          id="projectStatus"
-          name="projectStatus"
-          label="프로젝트 구성원 수"
-          margin={{ top: '48px' }}
-        >
-          <RadioButtonGroup
-            name="doc"
-            options={['one', 'two']}
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-          />
-        </FormField>
-        
-        <Button
-          type="submit"
-          label="완료"
-          primary={true}
-          margin={{ top: '48px' }}
-          size="large"
-          fill
-        />
-      </Form>
-    </>
+            <FormField
+              id="contact"
+              name="contact"
+              label="참여 연락 수단"
+              required={true}
+              margin={{ top: '48px' }}
+            >
+              <TextArea placeholder="프로젝트에 참여하기 위한 연락 수단을 알려주세요. (ex.이메일)" />
+            </FormField>
+
+            <FormField
+              id="reference"
+              name="reference"
+              label="참고 링크"
+              margin={{ top: '48px' }}
+            >
+              <TextArea placeholder="프로젝트에 참여 전 참고할만한 링크를 알려주세요. (ex.나의 프로필, 진행중인 프로젝트 링크 등)" />
+            </FormField>
+
+            <Button
+              type="submit"
+              label="완료"
+              primary={true}
+              margin={{ top: '48px' }}
+              size="large"
+              fill
+            />
+          </Form>
+        </div>
+      )}
+    </ResponsiveContext.Consumer>
   );
 }
 
